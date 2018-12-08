c3e5767c7c9d1455dd5652751308d6ef80035405
Stephen Ouyang <xisouyang@gmail.com>
2018-12-08 00:12:03 -0800

    autoLayouts for detailed habit view functional

--- Habitual/Views/HabitDetailedViewController.swift
+++ Habitual/Views/HabitDetailedViewController.swift
@@ -42,22 +42,28 @@ class HabitDetailedViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         updateUI()
-        stackTitleConstraints()
         imageConstraint()
-        buttonConstraint()
         streakStackConstraint()
+        stackTitleConstraints()
+        stackValuesConstraints()
+        startingDateConstraint()
+        buttonConstraint()
     }
     
     private func updateUI() {
-        print("here")
         title = habit.title
         imageViewIcon.image = habit.selectedImage.image
-        labelCurrentStreak.text = "\(habit.currentStreak) days"
+        
+        if habit.currentStreak == 1 {
+            labelCurrentStreak.text = "\(habit.currentStreak) day"
+        } else {
+            labelCurrentStreak.text = "\(habit.currentStreak) days"
+        }
+        
         labelTotalCompletions.text = String(habit.numberOfCompletions)
         labelBestStreak.text = String(habit.bestStreak)
         labelStartingDate.text = habit.dateCreated.stringValue
         if habit.hasCompletedToday {
-            print("BOO")
             buttonAction.setTitle("Completed for today", for: .normal)
         } else {
             buttonAction.setTitle("Mark as Completed", for: .normal)
@@ -76,36 +82,53 @@ class HabitDetailedViewController: UIViewController {
 
 extension HabitDetailedViewController {
     
-    func stackTitleConstraints() {
-        
-        stackTitles.translatesAutoresizingMaskIntoConstraints = false
-        stackTitles.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -120).isActive = true
-        stackTitles.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
-    }
-    
     func imageConstraint() {
         
         imageViewIcon.translatesAutoresizingMaskIntoConstraints = false
+        
         imageViewIcon.widthAnchor.constraint(equalToConstant: 250).isActive = true
         imageViewIcon.heightAnchor.constraint(equalTo: imageViewIcon.widthAnchor, multiplier: 1).isActive = true
         
         imageViewIcon.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
-        imageViewIcon.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -180).isActive = true
+        imageViewIcon.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: -160).isActive = true
+    }
+
+    func streakStackConstraint() {
+
+        streakStack.translatesAutoresizingMaskIntoConstraints = false
+
+        streakStack.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor, constant: 25).isActive = true
+        streakStack.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor, constant: 0).isActive = true
+    }
+
+    func stackTitleConstraints() {
+
+        stackTitles.translatesAutoresizingMaskIntoConstraints = false
+
+        stackTitles.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -120).isActive = true
+        stackTitles.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor, constant: 10).isActive = true
+    }
+    
+    func stackValuesConstraints() {
+        stackValues.translatesAutoresizingMaskIntoConstraints = false
+
+        stackValues.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -170).isActive = true
+        stackValues.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
+    }
+    
+    func startingDateConstraint() {
+        labelStartingDate.translatesAutoresizingMaskIntoConstraints = false
+
+        labelStartingDate.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -120).isActive = true
+        labelStartingDate.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor, constant: -20).isActive = true
     }
     
     func buttonConstraint() {
         buttonAction.translatesAutoresizingMaskIntoConstraints = false
-        
+
         buttonAction.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
         buttonAction.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor).isActive = true
         buttonAction.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, constant: -15).isActive = true
         buttonAction.heightAnchor.constraint(equalToConstant: 50).isActive = true
-    }
-    
-    func streakStackConstraint() {
-        
-        streakStack.translatesAutoresizingMaskIntoConstraints = false
-        streakStack.centerYAnchor.constraint(equalToSystemSpacingBelow: view.safeAreaLayoutGuide.centerYAnchor, multiplier: 1).isActive = true
-        streakStack.centerXAnchor.constraint(equalToSystemSpacingAfter: view.safeAreaLayoutGuide.centerXAnchor, multiplier: 1).isActive = true
     }
 }
